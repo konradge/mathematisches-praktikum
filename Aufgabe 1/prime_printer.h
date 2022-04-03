3e627a7c5f4d5e7fc5a621742d3f254572789103
konradge <gellerkonrad@gmail.com>
2022-04-03 17:12:40 +0200

    Modularized print function

--- Aufgabe 1/prime_printer.h
+++ Aufgabe 1/prime_printer.h
@@ -5,13 +5,12 @@
 #include <string>
 #include <vector>
 
-void print() {
+void print(int prime_count) {
   const int kOrdmax = 30;
-  const int kM = 300;
   const int kRr = 50;
   const int kCc = 4;
 
-  std::vector<int> p(kM + 1);
+  std::vector<int> p(prime_count + 1);
   p[1] = 2;
 
   std::vector<int> mult(kOrdmax + 1);
@@ -19,7 +18,7 @@ void print() {
   int ord = 2;
   int j = 1;
 
-  for (int k = 1; k < kM; k++) {
+  for (int k = 1; k < prime_count; k++) {
     bool j_prime;
     do {
       j += 2;
@@ -47,16 +46,16 @@ void print() {
   int page_number = 1;
   int page_offset = 1;
 
-  while (page_offset <= kM) {
+  while (page_offset <= prime_count) {
     std::cout << "The First ";
-    std::cout << kM;
+    std::cout << prime_count;
     std::cout << " Prime Numbers --- Page ";
     std::cout << page_number;
     std::cout << std::endl;
     for (int row_offset = page_offset; row_offset <= page_offset + kRr - 1;
          row_offset++) {
       for (int c = 0; c <= kCc - 1; c++) {
-        if (row_offset + c * kRr <= kM) {
+        if (row_offset + c * kRr <= prime_count) {
           std::cout << std::setw(10) << p[row_offset + c * kRr];
         }
       }

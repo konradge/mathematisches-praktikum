f6db6d9742615000bd367076708b3d01426842a4
Valdes Dekstop <valdesvoufo8@gmail.com>
2022-04-15 16:59:29 +0200

    bugfix

--- 02_sort/example_test.cpp
+++ 02_sort/example_test.cpp
@@ -10,6 +10,6 @@ int main() {
   mapra::MapraTest test("Beispiel");
   test.Assert("cin works", std::cin.good());
   test.AssertEq("max(-4,3) is 3", 3, std::max(-4, 3));
-  test.AssertEq("max(3,-4) is 3", 2, std::max(3, -4));  // extra falsch
+  test.AssertEq("max(3,-4) is 3", 3, std::max(3, -4));  // extra falsch
   return 0;
 }

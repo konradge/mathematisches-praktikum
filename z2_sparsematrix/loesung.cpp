431f00ee39a56988caf08df481bbecd7d7ed1a2e
Mustafa Semercio <mustafa.semercio@rwth-aachen.de>
2022-06-03 15:08:05 +0200

    Added stream-operators

--- z2_sparsematrix/loesung.cpp
+++ z2_sparsematrix/loesung.cpp
@@ -2,8 +2,23 @@
 
 #include "sparse_matrix.h"
 #include "vector.h"
+#include "unit.h"
 
 #define K_MAX 200
+
+bool isSymmetric(Sparse_Matrix& A) {
+
+  if (A.getCols() != A.getRows())
+    return false;
+    
+  for (size_t i = 0; i < A.getCols(); i++) {
+    for (size_t j = 0; j < i; j++) {
+      if(A(i,j) != A(j,i)) return false;
+    }
+  }
+  return true;
+  
+}
 
 double residuum(Sparse_Matrix& A, Vector& x, Vector& b) {
   return (b - A * x).norm2();
@@ -36,17 +51,24 @@ int gsv(Sparse_Matrix A, Vector& b, Vector& x0, const int k_max, double eps) {
   return 0;
 }
 
-void cg(Sparse_Matrix A, Vector& b, Vector& x0, const int k_max, double eps) {
+int cg(Sparse_Matrix A, Vector& b, Vector& x0, const int k_max, double eps) {
+  if (!isSymmetric(A)) {
+    std::cerr << "Matrix not symmetric" << std::endl;
+    return 0;
+  }
+  if (A.getCols() != b.getLength() || b.getLength() != x0.getLength()) {
+    return -1;    
+  }
   Vector r = b - A * x0;
-  Vector d(r), x(x0);
+  Vector d(r);
   double alpha, beta;
   double norm_r_squared = pow(r.norm2(), 2);
-
-  for (int k = 0; (k < K_MAX) && (residuum(A, x, b) > eps); k++) {
+  int k;
+  for (k = 0; (k < K_MAX) && (residuum(A, x0, b) > eps); k++) {
     Vector Ad(A * d);
     double energy_scalar_product = Ad * Ad;
     alpha = norm_r_squared / energy_scalar_product;
-    x += alpha * d;
+    x0 += alpha * d;
     r -= alpha * Ad;
     double norm_r_squared_next = pow(r.norm2(), 2);
     beta = norm_r_squared_next / norm_r_squared;
@@ -54,4 +76,17 @@ void cg(Sparse_Matrix A, Vector& b, Vector& x0, const int k_max, double eps) {
     d += r;
     norm_r_squared = norm_r_squared_next;
   }
+  return k;
+}
+
+int main() {
+  Sparse_Matrix A;
+  Vector x0, b;
+  double tol;
+  int max_iter;
+  getExample(2,A,x0,b, tol, max_iter);
+  int res = cg(A, b, x0, max_iter, tol);
+  checkSolution(x0, res, 1);
+
+  return 0;
 }
